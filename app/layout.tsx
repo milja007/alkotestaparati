f799a87b66adf0df50d9492e660d9ab053f01c3e
Borna Miljković <borna.milja@gmail.com>
2025-10-10 16:30:03 +0200

    metada

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,12 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Dr. Promil - Alkotest Aparati i Besplatni Online Kalkulator Promila",
   description:
-    "Testirajte razinu alkohola u krvi za samo 1-2€ na našim alkotest aparatima u pubovima, klubovima i kafićima u Hrvatskoj. Koristite besplatni online kalkulator promila prije izlaska ili usporedite rezultate s aparatom.",
+    "Testirajte razinu alkohola u krvi  na našim alkotest aparatima u pubovima, klubovima i kafićima u Hrvatskoj. Koristite besplatni online kalkulator promila prije izlaska ili usporedite rezultate s aparatom.",
   openGraph: {
     title:
       "Dr. Promil - Alkotest Aparati i Besplatni Online Kalkulator Promila",
     description:
-      "Testirajte razinu alkohola u krvi za samo 1-2€ na našim alkotest aparatima u pubovima, klubovima i kafićima u Hrvatskoj. Koristite besplatni online kalkulator promila prije izlaska ili usporedite rezultate s aparatom. Higijenski, brzo i precizno. Vlasnici lokala - pridružite se našem programu besplatno! Zajedno činimo noćni život odgovornijim.",
+      "Testirajte razinu alkohola u krvi na našim alkotest aparatima u pubovima, klubovima i kafićima u Hrvatskoj. Koristite besplatni online kalkulator promila.",
     type: "website",
     locale: "hr_HR",
     siteName: "Dr. Promil",
